81f48d863a83db5d2b06f410a3a5448669ab2ae2
mmichael255 <cby371@gmail.com>
2024-10-20 10:35:29 +0900

    finished header motion

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,15 @@
-import React, { useRef } from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
   return (
-    <section>
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
